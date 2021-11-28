149817fc60dacb97aa06802a674c71c01a2a3260
MatiCava <matiascavallin96@gmail.com>
2021-11-28 03:37:35 -0500

    sales como carrito y test

--- api/src/routes/Sales/SalesController.ts
+++ api/src/routes/Sales/SalesController.ts
@@ -4,39 +4,75 @@ import WrapperProduct from "../WrapperProducts/WrapperProduct"
 import User from "../Users/User"
 import Sale from "./Sales"
 import ValidateAuthService from "../ValidateAuthService"
+import { ObjectId } from "mongoose"
+
+const checkProducts = (products, idProdsCantidad) => {
+    const result = {}
+    result['stock'] = false
+    result['owner'] = false
+    for(var product of products){
+        result['stock'] = result['stock'] || product.get('stock') < idProdsCantidad[product.get('_id')]
+        result['owner'] = result['owner'] || !product.get('owner').get('seller')
+    }
+    return result
+}
 
 export const createSale: RequestHandler = async (req, res) => {
     let uid = await ValidateAuthService.validateAuth(req,res)
     if(!uid){
         return res.status(403).json({message: "Unauthorized"})
     }
-    if(!req.body.formaPago || !req.body.product || !req.body.buyer || !req.body.taxes)
-        return res.status(400).json({message: 'Missing required field, check formaPago product buyer and taxes should not be empty.'})
+    if(!req.body.formaPago || !req.body.buyer || !req.body.costoTotal || !req.body.products)
+        return res.status(400).json({message: 'Missing required field, check formaPago products buyer and costoTotal products should not be empty.'})
    
-    const buyerExists = await User.findById(req.body.buyer)
-    let productExists = await Product.findById(req.body.product)
-   
-    if(!buyerExists || !productExists || productExists.get('stock') <= 0)
-        return res.status(400).json({message: 'Buyer or product does not exist for this sale or product is out of stock.'})
-
-    const sellerExists = await User.findById(productExists.get('owner'))
-    if(!sellerExists || !sellerExists.get('seller'))
-        return res.status(400).json({message: 'Owner of the product is not a seller or does not exist.'})
-
-    const wrapperProd = new WrapperProduct({
-        nombre: productExists.get('nombre'),
-        valor: productExists.get('valor'),
-        owner: productExists.get('owner'),
-        categoria: productExists.get('categoria')
+    var prodIds : ObjectId[] = []
+    var prodIdCantidad = {}
+    
+    req.body.products.forEach(function (prodSchema) {
+        prodIds.push(prodSchema.productId)
+        prodIdCantidad[prodSchema.productId] = prodSchema.cantidadProductos
     })
-    const savedWrapperProd = await wrapperProd.save()
-    let sale = new Sale(req.body)
-    sale.set({ wrapperProduct: savedWrapperProd.get('_id') })
-    sale.set({ seller: productExists.get('owner') })
-    const savedSale = await sale.save()
-    productExists.set({ stock: productExists.get('stock') - 1 })
-    await productExists.save()
-    res.json(savedSale)
+    const buyerExists = await User.findById(req.body.buyer)
+    let allProducts = await Product.find({
+        '_id':{ $in: prodIds }
+    }).populate('owner')
+
+    const resultCheck = checkProducts(allProducts, prodIdCantidad)
+    
+    if(!buyerExists || resultCheck['stock'])
+        return res.status(400).json({message: 'Buyer or one of the products does not exist for this sale or product is out of stock.'})
+
+    if(resultCheck['owner'])
+        return res.status(400).json({message: 'Owner of one of the product is not a seller or does not exist.'})
+
+    var wrappersId : ObjectId[] = []
+    var ownerIds : ObjectId[] = []
+    var processProduct = new Promise<void>( (resolve, reject) => { 
+        allProducts.forEach(async function (prod, index, array) {
+            const wrapperProd = new WrapperProduct({
+                nombre: prod.get('nombre'),
+                valor: prod.get('valor'),
+                owner: prod.get('owner'),
+                categoria: prod.get('categoria')
+            })
+            const savedWrapperProd = await wrapperProd.save()
+
+            wrappersId.push(savedWrapperProd.get('_id'))
+            ownerIds.push(prod.get('owner'))
+    
+            prod.set({ stock: prod.get('stock') - prodIdCantidad[prod.get('_id')] })
+            await prod.save()
+            if(index === array.length - 1) resolve()
+        })
+    })
+    
+    processProduct.then( async () => {
+        let sale = new Sale(req.body)
+        sale.set({ wrapperProduct: wrappersId })
+        sale.set({ seller: ownerIds })
+        const savedSale = await sale.save()
+        res.json(savedSale)
+    })
 }
 
 export const getSale: RequestHandler = async (req, res) => {
---
+++ api/src/tests/sales.test.ts
@@ -0,0 +1,440 @@
+import app from '../app'
+import request from 'supertest'
+import User from '../routes/Users/User'
+import Product from '../routes/Products/Product'
+import Sale from '../routes/Sales/Sales'
+import WrapperProduct from '../routes/WrapperProducts/WrapperProduct'
+import ValidateAuthService from '../routes/ValidateAuthService'
+
+describe('create sale', () => {
+
+    beforeAll(async () => {
+        User.findById = jest.fn().mockReturnValue({
+            wallet: 0,
+            _id: "6150d5b7f3a8afa65c28d633",
+            nombre: 'hello world user1',
+            apellido: 'apellido test',
+            username: 'username2',
+            password: 'password1',
+            seller: true,
+            email: 'email2',
+            razonSocial: '1',
+            emailCorporativo: 'emailC2'
+        })
+        Product.find = jest.fn().mockReturnValue({populate: () => {
+            var res : any[] = []
+            const newProd = new Product({
+                _id: "6150d62cf3a8afa65c28d642",
+                nombre: 'Test',
+                descripcion: 'test desc',
+                valor: 50,
+                stock: 24,
+                owner: new User({
+                    wallet: 0,
+                    _id: "6150d616f3a8afa65c28d63c",
+                    nombre: 'cambia nombre opa',
+                    apellido: 'apellido test',
+                    username: 'username1',
+                    password: 'password1',
+                    seller: true,
+                    email: 'email1',
+                    razonSocial: '12',
+                    emailCorporativo: 'emailC1'
+                }),
+                categoria: 'Mascotas'
+            })
+            res.push(newProd)
+            return res
+         } })
+        ValidateAuthService.validateAuth = jest.fn().mockReturnValue(1)
+
+    });
+
+
+    test('post new sale returning status 200', async () => {
+
+        WrapperProduct.prototype.save = jest.fn().mockImplementation( () => {
+            
+            const savedWrapperProd =  new WrapperProduct({
+                nombre: 'Test',
+                valor: 50,
+                owner: {
+                  wallet: 0,
+                  _id: "6150d616f3a8afa65c28d63c",
+                  nombre: 'cambia nombre opa',
+                  apellido: 'apellido test',
+                  username: 'username1',
+                  password: 'password1',
+                  seller: true,
+                  email: 'email1',
+                  razonSocial: '12',
+                  emailCorporativo: 'emailC1'
+                },
+                categoria: 'Mascotas',
+                _id: "61a28f721d4f8f11155b28bc"
+              })
+
+            return savedWrapperProd
+        })
+        
+        Product.prototype.save = jest.fn().mockImplementation( () => {
+            
+            const prod =  new Product({
+                nombre: 'Test',
+                descripcion: 'test desc',
+                valor: 50,
+                stock: 21,
+                owner: {
+                  wallet: 0,
+                  _id: "6150d616f3a8afa65c28d63c",
+                  nombre: 'cambia nombre opa',
+                  apellido: 'apellido test',
+                  username: 'username1',
+                  password: 'password1',
+                  seller: true,
+                  email: 'email1',
+                  razonSocial: '12',
+                  emailCorporativo: 'emailC1'
+                },
+                categoria: 'Mascotas',
+                _id: "6150d62cf3a8afa65c28d642"
+              })
+
+            return prod
+        })
+
+        Sale.prototype.save = jest.fn().mockImplementation( () => {
+            
+            const sale =  new Sale({
+                costoTotal: 66,
+                buyer: "6150d5b7f3a8afa65c28d633",
+                seller: [
+                    {
+                        wallet: 0,
+                        _id: "6150d616f3a8afa65c28d63c",
+                        nombre: "cambia nombre opa",
+                        apellido: "apellido test",
+                        username: "username1",
+                        password: "password1",
+                        seller: true,
+                        email: "email1",
+                        razonSocial: "12",
+                        emailCorporativo: "emailC1",
+                        createdAt: "2021-09-26T20:20:38.907Z",
+                        updatedAt: "2021-09-26T20:20:50.038Z"
+                    }
+                ],
+                products: [
+                    {
+                        cantidadProductos: 1,
+                        productId: "6150d62cf3a8afa65c28d642",
+                        _id: "61a28b9217a539b69737940b"
+                    }
+                ],
+                wrapperProduct: [
+                    "61a28b9217a539b697379409"
+                ],
+                formaPago: "Card",
+                _id: "61a28b9217a539b69737940a"
+            })
+
+            return sale
+        })
+        
+        const response = await request(app).post('/sales').send({
+            costoTotal: 66,
+            products: [
+                {
+                    productId: "6150d62cf3a8afa65c28d642",
+                    cantidadProductos: 1
+                }
+            ],
+            buyer: "6150d5b7f3a8afa65c28d633",
+            formaPago: "Card"
+        })
+
+        expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
+        expect(response.statusCode).toBe(200)
+    })
+
+    test('post new sale without costototal returning status 400 missing required fields', async () => {
+        
+        const response = await request(app).post('/sales').send({
+            products: [
+                {
+                    productId: "6150d62cf3a8afa65c28d642",
+                    cantidadProductos: 1
+                }
+            ],
+            buyer: "6150d5b7f3a8afa65c28d633",
+            formaPago: "Card"
+        })
+
+        expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
+        expect(response.statusCode).toBe(400)
+        expect(response.text).toEqual(expect.stringContaining('Missing required field, check formaPago products buyer and costoTotal products should not be empty.'))
+    })
+})
+
+describe('get sale', () => {
+
+    beforeAll(async () => {
+        ValidateAuthService.validateAuth = jest.fn().mockReturnValue(1)
+    });
+
+
+    test('get sales returning status 200', async () => {
+
+        const idSale = '6150d62cf3a8afa65c28d642'
+
+        Sale.find = jest.fn().mockReturnValue({populate: () => {
+            var res : any[] = []
+            const newSale = new Sale({
+                _id: idSale,
+                costoTotal: 66,
+                buyer: {
+                wallet: 0,
+                _id: "6150d5b7f3a8afa65c28d633",
+                nombre: "hello world user1",
+                apellido: "apellido test",
+                username: "username2",
+                password: "password1",
+                seller: true,
+                email: "email2",
+                razonSocial: "1",
+                emailCorporativo: "emailC2"
+                },
+                seller: [
+                {
+                    wallet: 0,
+                    _id: "6150d616f3a8afa65c28d63c",
+                    nombre: "cambia nombre opa",
+                    apellido: "apellido test",
+                    username: "username1",
+                    password: "password1",
+                    seller: true,
+                    email: "email1",
+                    razonSocial: "12",
+                    emailCorporativo: "emailC1"
+                }
+                ],
+                products: [
+                {
+                    cantidadProductos: 1,
+                    productId: "6150d62cf3a8afa65c28d642",
+                    _id: "61a33040abb4ebd8549da413"
+                }
+                ],
+                wrapperProduct: [
+                {
+                    _id: "61a33040abb4ebd8549da40f",
+                    nombre: "Test",
+                    valor: 50,
+                    owner: "6150d616f3a8afa65c28d63c",
+                    categoria: "Mascotas"
+                }
+                ],
+                formaPago: "Card"
+            })
+            res.push(newSale)
+            return res
+        }})
+        
+        const response = await request(app).get('/sales').send()
+
+        expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
+        expect(response.statusCode).toBe(200)
+    })
+
+    test('get sale returning status 200', async () => {
+
+        const idSale = '6150d62cf3a8afa65c28d642'
+
+        Sale.findById = jest.fn().mockReturnValue({populate: () => {
+            var res : any[] = []
+            const newSale = new Sale({
+                _id: idSale,
+                costoTotal: 66,
+                buyer: {
+                wallet: 0,
+                _id: "6150d5b7f3a8afa65c28d633",
+                nombre: "hello world user1",
+                apellido: "apellido test",
+                username: "username2",
+                password: "password1",
+                seller: true,
+                email: "email2",
+                razonSocial: "1",
+                emailCorporativo: "emailC2"
+                },
+                seller: [
+                {
+                    wallet: 0,
+                    _id: "6150d616f3a8afa65c28d63c",
+                    nombre: "cambia nombre opa",
+                    apellido: "apellido test",
+                    username: "username1",
+                    password: "password1",
+                    seller: true,
+                    email: "email1",
+                    razonSocial: "12",
+                    emailCorporativo: "emailC1"
+                }
+                ],
+                products: [
+                {
+                    cantidadProductos: 1,
+                    productId: "6150d62cf3a8afa65c28d642",
+                    _id: "61a33040abb4ebd8549da413"
+                }
+                ],
+                wrapperProduct: [
+                {
+                    _id: "61a33040abb4ebd8549da40f",
+                    nombre: "Test",
+                    valor: 50,
+                    owner: "6150d616f3a8afa65c28d63c",
+                    categoria: "Mascotas"
+                }
+                ],
+                formaPago: "Card"
+            })
+            res.push(newSale)
+            return res
+        }})
+        
+        const response = await request(app).get('/sale/' + idSale).send()
+
+        expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
+        expect(response.statusCode).toBe(200)
+    })
+})
+
+describe('delete sale', () => {
+
+    beforeAll(async () => {
+        ValidateAuthService.validateAuth = jest.fn().mockReturnValue(1)
+    });
+
+    test('delete sale returning status 200', async () => {
+
+        const idSale = '6150d62cf3a8afa65c28d642'
+
+        Sale.findByIdAndDelete = jest.fn().mockReturnValue({
+            _id: idSale,
+            costoTotal: 66,
+            buyer: {
+              wallet: 0,
+              _id: "6150d5b7f3a8afa65c28d633",
+              nombre: "hello world user1",
+              apellido: "apellido test",
+              username: "username2",
+              password: "password1",
+              seller: true,
+              email: "email2",
+              razonSocial: "1",
+              emailCorporativo: "emailC2"
+            },
+            seller: [
+              {
+                wallet: 0,
+                _id: "6150d616f3a8afa65c28d63c",
+                nombre: "cambia nombre opa",
+                apellido: "apellido test",
+                username: "username1",
+                password: "password1",
+                seller: true,
+                email: "email1",
+                razonSocial: "12",
+                emailCorporativo: "emailC1"
+              }
+            ],
+            products: [
+              {
+                cantidadProductos: 1,
+                productId: "6150d62cf3a8afa65c28d642",
+                _id: "61a33040abb4ebd8549da413"
+              }
+            ],
+            wrapperProduct: [
+              {
+                _id: "61a33040abb4ebd8549da40f",
+                nombre: "Test",
+                valor: 50,
+                owner: "6150d616f3a8afa65c28d63c",
+                categoria: "Mascotas"
+              }
+            ],
+            formaPago: "Card"
+        })
+        
+        const response = await request(app).delete('/sales/' + idSale).send()
+        expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
+        expect(response.statusCode).toBe(200)
+        expect(response.text).toEqual(expect.stringContaining('Sale deleted'))
+    })
+})
+
+describe('update sale', () => {
+
+    beforeAll(async () => {
+        ValidateAuthService.validateAuth = jest.fn().mockReturnValue(1)
+    });
+
+    test('update sale returning status 200', async () => {
+
+        const idSale = '6150d62cf3a8afa65c28d642'
+
+        Sale.findByIdAndUpdate = jest.fn().mockReturnValue({
+            _id: idSale,
+            costoTotal: 66,
+            buyer: {
+              wallet: 0,
+              _id: "6150d5b7f3a8afa65c28d633",
+              nombre: "hello world user1",
+              apellido: "apellido test",
+              username: "username2",
+              password: "password1",
+              seller: true,
+              email: "email2",
+              razonSocial: "1",
+              emailCorporativo: "emailC2"
+            },
+            seller: [
+              {
+                wallet: 0,
+                _id: "6150d616f3a8afa65c28d63c",
+                nombre: "cambia nombre opa",
+                apellido: "apellido test",
+                username: "username1",
+                password: "password1",
+                seller: true,
+                email: "email1",
+                razonSocial: "12",
+                emailCorporativo: "emailC1"
+              }
+            ],
+            products: [
+              {
+                cantidadProductos: 1,
+                productId: "6150d62cf3a8afa65c28d642",
+                _id: "61a33040abb4ebd8549da413"
+              }
+            ],
+            wrapperProduct: [
+              {
+                _id: "61a33040abb4ebd8549da40f",
+                nombre: "Test",
+                valor: 50,
+                owner: "6150d616f3a8afa65c28d63c",
+                categoria: "Mascotas"
+              }
+            ],
+            formaPago: "Card"
+        })
+        
+        const response = await request(app).put('/sales/' + idSale).send()
+        expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
+        expect(response.statusCode).toBe(200)
+    })
+})
