311cbe32bcad0b8fc5fff9335cefd59f04c66ae2
MatiCava <matiascavallin96@gmail.com>
2021-11-29 12:43:13 -0500

    ajuste app

--- api/src/app.ts
+++ api/src/app.ts
@@ -12,10 +12,8 @@ app.set('port', config.PORT);
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}))
+app.use(express.urlencoded({limit: '50mb', extended: false}));
 
 app.use(productsRoutes);
 app.use(userRoutes);
